c116a8199961d97cbdda02130d28e9d9b5fa720a
FuscaoPreto <mywork.anon@protonmail.com>
2024-04-10 12:34:52 -0300

    Criando o repositório

---
+++ main.py
@@ -0,0 +1,47 @@
+import time
+
+LIMITE_SAQUE=500
+SAQUES_MAX=3
+numero_de_saques=0
+
+saldo=0
+
+operacao=""
+
+while operacao != "x":
+    operacao=input(
+'''
+Operações disponíveis:
+                   
+Saque (s)
+                   
+Depósito (d)
+                   
+Extrato (e)
+                
+Sair (x)
+                   
+'''                 )
+    
+    if operacao == "s":
+        if numero_de_saques < SAQUES_MAX:
+            valor_sacado=float(input("Digite o valor a sacar: "))
+            if valor_sacado > saldo:
+                print("O valor sacado é superior ao saldo disponível")
+            elif valor_sacado > LIMITE_SAQUE:
+                print("Valor a ser sacado excede o limite de saque")
+            else:
+                saldo-=valor_sacado
+                numero_de_saques+=1
+                print(f"Novo Saldo: R${saldo:.2f}")
+        else:
+            print("Numero máximo de saques diários foi atingido (3)")
+    elif operacao == "d":
+        valor_deposito=float(input("Digite o valor a depositar: "))
+        saldo+=valor_deposito
+        print(f"Novo Saldo: R${saldo:.2f}")
+    elif operacao == "e":
+        print(f"Saldo: R${saldo:.2f}")
+    else:
+        continue
+    time.sleep(1)
